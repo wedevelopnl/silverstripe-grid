551dbf17bcffef6f9a79e136a04959e78aae0e6e
Erik Frèrejean <erik@wedevelop.nl>
2026-03-13 15:48:47 +0100

    feat: add DuplicateToRequest value object

---
+++ src/Value/DuplicateToRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeDevelop\Grid\Value;
+
+final readonly class DuplicateToRequest
+{
+    /**
+     * @param positive-int $id
+     * @param positive-int $targetPageId
+     * @param non-empty-string $targetZone
+     * @param positive-int $targetParentId
+     */
+    public function __construct(
+        public int $id,
+        public int $targetPageId,
+        public string $targetZone,
+        public int $targetParentId,
+    ) {
+    }
+}
